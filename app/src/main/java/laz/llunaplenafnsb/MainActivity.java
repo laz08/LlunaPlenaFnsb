36b6455f1a35aa7ecb2551cc6a7c273eddbf8913
Laura C <lazeru08@gmail.com>
2016-01-28 11:37:06 +0100

    Initial commit

---
+++ app/src/main/java/laz/llunaplenafnsb/MainActivity.java
@@ -0,0 +1,17 @@
+package laz.llunaplenafnsb;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Main activity.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
