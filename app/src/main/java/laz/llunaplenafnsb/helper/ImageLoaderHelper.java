4f5a648a66fb5878eb7e365fd517780368bd64f3
Laura C <lazeru08@gmail.com>
2016-05-26 13:32:18 +0200

    Image loader hides imageview on error

--- app/src/main/java/laz/llunaplenafnsb/helper/ImageLoaderHelper.java
+++ app/src/main/java/laz/llunaplenafnsb/helper/ImageLoaderHelper.java
@@ -1,8 +1,11 @@
 package laz.llunaplenafnsb.helper;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import laz.llunaplenafnsb.R;
@@ -12,6 +15,8 @@ import laz.llunaplenafnsb.R;
  */
 public class ImageLoaderHelper {
 
+    public static final String TAG = "ImageLoaderHelper";
+
     /**
      * Loads image from URL into imageview.
      *
@@ -19,9 +24,24 @@ public class ImageLoaderHelper {
      * @param imageView ImageView.
      * @param url       URL.
      */
-    public static void loadImageInto(Context ctx, ImageView imageView, String url) {
+    public static void loadImageInto(Context ctx, final ImageView imageView, String url) {
 
         //TODO: PlaceholdeR?
-        Picasso.with(ctx).load(url).error(R.drawable.ic_error_black_24dp).into(imageView);
+        Picasso.with(ctx).load(url)/*.error(R.drawable.ic_error_black_24dp)*/.into(imageView,
+                new Callback() {
+
+                    @Override
+                    public void onSuccess() {
+
+                        Log.v(TAG, "onSuccess");
+                    }
+
+                    @Override
+                    public void onError() {
+
+                        Log.v(TAG, "onError");
+                        imageView.setVisibility(View.GONE);
+                    }
+                });
     }
 }
